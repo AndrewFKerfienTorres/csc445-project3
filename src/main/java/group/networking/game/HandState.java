2c51cabb8d80fd97d76bfcc1e75da1e79d4dcc92
Alavaster <akerfien@Oswego.edu>
2026-05-02 13:40:10 -0400

    added description to hand states for future use.

--- src/main/java/group/networking/game/HandState.java
+++ src/main/java/group/networking/game/HandState.java
@@ -1,5 +1,14 @@
 package group.networking.game;
 
+
+/*                                          HAND STATE
+ *                         BELOW LIMIT -> value is <21, therefore player is good to go.
+ *                         LIMIT       -> value is 21, if turn 1 automatic win, else tell player they're at limit.
+ *                         ABOVE LIMIT -> value is >21, automatic loss for the player.
+ */
+
+
+
 public enum HandState {
     BELOW_LIMIT,
     LIMIT,
